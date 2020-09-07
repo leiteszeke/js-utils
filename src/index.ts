ccce542885207f0468613286b26400dfffaa6340
Ezequiel <ezequiel@leites.dev>
2020-09-07 19:38:29 +0200

    feat: expose http module

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import * as Numbers from './numbers';
 import * as Objects from './objects';
 import * as Utils from './utils';
 import * as Hooks from './hooks';
+import * as Http from './http';
 
 export { Numbers, Objects, Utils, Hooks };
