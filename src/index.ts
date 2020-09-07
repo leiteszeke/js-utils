47564a1bb0b6c1712e1919df67f5896ef728f067
Ezequiel <ezequiel@leites.dev>
2020-09-07 19:54:07 +0200

    feat: remove hooks

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import * as Numbers from './numbers';
 import * as Objects from './objects';
 import * as Utils from './utils';
-import * as Hooks from './hooks';
 import * as Http from './http';
 
-export { Numbers, Objects, Utils, Http, Hooks };
+export { Numbers, Objects, Utils, Http };
